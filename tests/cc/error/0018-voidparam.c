961ee6c2ea156ff13f4dc54ddde5f1e9e4cffe35
Roberto E. Vargas Caballero <k0ga@shike2.com>
2024-10-16 17:16:47 +0200

    tests/cc: Update 0018-voidparam
    After the commit be306281 this test has new detected errors.

--- tests/cc/error/0018-voidparam.c
+++ tests/cc/error/0018-voidparam.c
@@ -1,6 +1,9 @@
 /*
 PATTERN:
+0018-voidparam.c:9: error: 'void' must be the only parameter
+0018-voidparam.c:15: error: 'void' must be the only parameter
 0018-voidparam.c:27: error: a named argument is requiered before '...'
+0018-voidparam.c:27: error: 'void' must be the only parameter
 .
 */
 
