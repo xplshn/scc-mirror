a1d384d7e6679855b3ac78b188928aaef1ddff03
Roberto E. Vargas Caballero <k0ga@shike2.com>
2024-10-17 09:53:05 +0200

    tests/cc: Update 0018-voidparam.c and 0038-void.c

--- tests/cc/error/0038-void.c
+++ tests/cc/error/0038-void.c
@@ -1,15 +1,19 @@
 /*
 PATTERN:
-0038-void.c:18: error: 'void' must be the only parameter
-0038-void.c:24: error: 'void' must be the only parameter
-0038-void.c:30: error: 'void' must be the only parameter
-0038-void.c:45: error: too many arguments in function call
-0038-void.c:46: error: too few arguments in function call
-0038-void.c:48: error: too many arguments in function call
-0038-void.c:49: error: too few arguments in function call
+0038-void.c:22: error: 'void' must be the only parameter
+0038-void.c:27: warning: 'a' defined but not used
+0038-void.c:28: error: 'void' must be the only parameter
+0038-void.c:33: warning: 'a' defined but not used
+0038-void.c:34: error: 'void' must be the only parameter
+0038-void.c:39: warning: 'b' defined but not used
+0038-void.c:39: warning: 'a' defined but not used
+0038-void.c:49: error: too many arguments in function call
 0038-void.c:50: error: too few arguments in function call
-0038-void.c:52: error: too few arguments in function call
-0038-void.c:54: error: too many arguments in function call
+0038-void.c:52: error: too many arguments in function call
+0038-void.c:53: error: too few arguments in function call
+0038-void.c:54: error: too few arguments in function call
+0038-void.c:56: error: too few arguments in function call
+0038-void.c:58: error: too many arguments in function call
 too many errors
 .
 */
