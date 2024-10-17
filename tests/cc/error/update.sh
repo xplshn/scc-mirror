8bbde8d70865d86dfb70713f7c1304a6a2ef4de5
Roberto E. Vargas Caballero <k0ga@shike2.com>
2024-10-17 10:03:32 +0200

    tests/cc: Update twice in update.sh
    When running update.sh it can add/remove lines to the header
    that makes invalid all the numbers in the first execution,
    forcing to run it 2 times. As there were sereval commits updating
    several tests with this problem then is better to run the update
    twice in the onw script.

--- tests/cc/error/update.sh
+++ tests/cc/error/update.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
-for i
-do
-	(echo '/^PATTERN/+;/^\./-c'
-	 scc $CFLAGS -W -c $i 2>&1
-	 printf ".\nw\n"
-	 echo w) |
-	ed -s $i
-done
+update()
+{
+	for i
+	do
+		(echo '/^PATTERN/+;/^\./-c'
+		 scc $CFLAGS -W -c $i 2>&1
+		 printf ".\nw\n"
+		 echo w) |
+		ed -s $i
+	done
+}
+
+update $@
+update $@
