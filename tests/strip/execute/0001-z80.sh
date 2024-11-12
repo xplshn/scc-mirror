aa53f2a92c4ff4e2dab1c7af4f35c8745c78873b
Roberto E. Vargas Caballero <k0ga@shike2.com>
2024-11-12 22:30:49 +0100

    tests/strip: Use new scc wrapper in the tests
    This make easier to use scc-strip from other directories.

--- tests/strip/execute/0001-z80.sh
+++ tests/strip/execute/0001-z80.sh
@@ -8,11 +8,11 @@ tmp2=tmpfile2
 tmp3=tmpfile3
 
 cp z80.out $tmp1
-scc-strip $tmp1 > $tmp2
-z80-unknown-coff-nm $tmp1 >> $tmp2 2>&1 || true
+scc strip $tmp1 > $tmp2
+scc nm $tmp1 >> $tmp2 2>&1 || true
 
 cat > $tmp3 <<EOF
-z80-unknown-coff-nm: $tmp1: no symbols
+nm: $tmp1: no symbols
 EOF
 
 diff $tmp2 $tmp3
