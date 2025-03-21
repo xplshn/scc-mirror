36606c7df70f2465a4305395c529e7ea1add693f
Roberto E. Vargas Caballero <k0ga@shike2.net>
2025-03-21 19:23:17 +0100

    tests/libc: Add 0042-mbsrtowc

--- src/libc/wchar/mbsrtowcs.c
+++ src/libc/wchar/mbsrtowcs.c
@@ -1,4 +1,5 @@
 #include <limits.h>
+#include <string.h>
 #include <wchar.h>
 
 #undef mbsrtowcs
@@ -8,24 +9,39 @@ mbsrtowcs(wchar_t *restrict dest, const char **restrict src, size_t len,
           mbstate_t *restrict ps)
 {
 	wchar_t wc;
+	const char *s = *src;
 	size_t cnt, n;
+	static mbstate_t st;
+
+	if (!ps)
+		ps = &st;
 
 	for (n = 0; ; n++) {
-		cnt = mbrtowc(&wc, *src, MB_LEN_MAX, ps);
+		cnt = mbrtowc(&wc, s, MB_LEN_MAX, ps);
+		if (cnt == (size_t) -2) {
+			s += MB_LEN_MAX;
+			continue;
+		}
 		if (cnt == (size_t) -1)
 			return -1;
 
 		if (dest) {
-			if (n == len)
+			if (n == len) {
+				*src = s;
 				return n;
+			}
 			*dest++ = wc;
 		}
-		*src += cnt;
+		s += cnt;
 
-		if (wc == L'\0')
+		if (wc == 0)
 			break;
 	}
-	*src = NULL;
+
+	if (dest) {
+		memset(ps, 0, sizeof(mbstate_t));
+		*src = NULL;
+	}
 
 	return n;
 }
